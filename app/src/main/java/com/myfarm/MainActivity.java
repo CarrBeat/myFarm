3fab3408507fc2f9721ac786854e8f8b8d14af1c
CarrBeat <carrbeat@gmail.com>
2023-02-20 23:57:22 +0300

    categories not finished and not unbagged

--- app/src/main/java/com/myfarm/MainActivity.java
+++ app/src/main/java/com/myfarm/MainActivity.java
@@ -1,13 +1,46 @@
 package com.myfarm;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.myfarm.adapter.CategoryAdapter;
+import com.myfarm.model.Category;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView categoryRecycler;
+    CategoryAdapter categoryAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1, "Птицы"));
+        categoryList.add(new Category(2, "Рогатый скот"));
+        categoryList.add(new Category(3, "Иные млекопитающие"));
+        categoryList.add(new Category(4, "Прочие"));
+
+        setCategoryRecycler(categoryList);
+    }
+
+    @SuppressLint("WrongViewCast")
+    private void setCategoryRecycler(List<Category> categoryList) {
+
+        // организуем горизонтальный вывод:
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this,
+                RecyclerView.HORIZONTAL, false);
+        categoryRecycler = findViewById(R.id.category_title);
+        categoryRecycler.setLayoutManager(layoutManager);
+
+        categoryAdapter = new CategoryAdapter(this, categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
     }
 }
