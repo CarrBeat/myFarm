c3e424189ec9a16c787febed26aa634656ec2705
CarrBeat <carrbeat@gmail.com>
2023-04-21 13:06:16 +0300

    AnimalRepository можно брать за основу других

--- app/src/main/java/com/myfarm/db/AnimalRepository.java
+++ app/src/main/java/com/myfarm/db/AnimalRepository.java
@@ -5,15 +5,15 @@ import android.app.Application;
 import java.util.List;
 
 public class AnimalRepository {
-    private AnimalDao tAnimalDao;
-    private List<Animal> AnimalObjects;
+    private final AnimalDao tAnimalDao;
 
 
     AnimalRepository(Application application) {
         AnimalDatabase db = AnimalDatabase.getDatabase(application);
         tAnimalDao = db.animalDao();
-        AnimalObjects = tAnimalDao.getAllAnimals();
     }
 
-
+    public List<Animal> getAllAnimals(){
+        return tAnimalDao.getAllAnimals();
+    }
 }
--- app/src/main/java/com/myfarm/db/PregnancyDao.java
+++ app/src/main/java/com/myfarm/db/PregnancyDao.java
@@ -1,11 +1,13 @@
 package com.myfarm.db;
 
+import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 
 import java.util.List;
 
+@Dao
 public interface PregnancyDao {
     @Insert
     void insertAll(Pregnancy... pregnancies);
