239364c3af8eae6195dc2bea023f0ecc795aa15f
CarrBeat <carrbeat@gmail.com>
2023-04-22 00:03:05 +0300

    подправил косячки, далее - связи в БД

--- app/src/main/java/com/myfarm/db/ProductivityDao.java
+++ app/src/main/java/com/myfarm/db/ProductivityDao.java
@@ -6,6 +6,7 @@ import androidx.room.Insert;
 import androidx.room.Query;
 import java.util.List;
 
+@Dao
 public interface ProductivityDao {
     @Insert
     void insertAll(Productivity... productivities);
--- app/src/main/java/com/myfarm/db/ProductivityDatabase.java
+++ app/src/main/java/com/myfarm/db/ProductivityDatabase.java
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Database(entities = {ProductivityDatabase.class}, version = 1, exportSchema = false)
+@Database(entities = {Productivity.class}, version = 1, exportSchema = false)
 public abstract class ProductivityDatabase extends RoomDatabase {
     public abstract ProductivityDao productivityDao();
 
