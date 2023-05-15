375c367d14f585d46ca6aac95cdf3975b76ffe26
CarrBeat <carrbeat@gmail.com>
2023-05-15 17:42:31 +0300

    БД и MainActivity.java с AnimalsFragment.java буду изменять

--- app/src/main/java/com/myfarm/AnimalsFragment.java
+++ app/src/main/java/com/myfarm/AnimalsFragment.java
@@ -1,6 +1,8 @@
 package com.myfarm;
 
+import android.arch.lifecycle.ViewModelProvider;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,11 +10,19 @@ import android.view.ViewGroup;
 
 
 public class AnimalsFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable @androidx.annotation.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //AnimalViewModel animalViewModel = new ViewModelProvider(this).get(AnimalViewModel.class);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_animals, container, false);
+        View view = inflater.inflate(R.layout.fragment_animals, container, false);
+        view.findViewById(R.id.recycler_view);
+        return view;
     }
 }
