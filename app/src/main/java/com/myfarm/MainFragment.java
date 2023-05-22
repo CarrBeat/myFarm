0603eb7803adbd3621eac0bb699d47d1eb25eee8
CarrBeat <carrbeat@gmail.com>
2023-05-22 11:22:44 +0300

    spinner в норме

--- app/src/main/java/com/myfarm/MainFragment.java
+++ app/src/main/java/com/myfarm/MainFragment.java
@@ -4,14 +4,52 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import androidx.fragment.app.Fragment;
 
-public class MainFragment extends Fragment {
+import com.myfarm.db.Animal;
+import com.myfarm.db.MyFarmDatabase;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class MainFragment extends Fragment {
+    String[] animalNames;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Spinner spinner = view.findViewById(R.id.select_animal_spinner);
+
+        List<Animal> animals = MyFarmDatabase.getDatabase(getContext()).animalDao().getAllAnimals();
+        List<String> animalTypes = MyFarmDatabase.getDatabase(getContext()).animalTypeDao().getAnimalTypeNames();
+        Animal currentAnimal;
+
+        Map<String, Integer> animalsMap = new HashMap<>();;
+        ArrayList<String> animalArray = new ArrayList<>();
+
+        for (int i = 0; i < animals.size(); i++){
+            currentAnimal = animals.get(i);
+
+            if(!Objects.equals(currentAnimal.getAnimalName(), "")){
+                animalArray.add(animalTypes.get(currentAnimal.getAnimalTypeID()) + currentAnimal.getAnimalName());
+            } else {
+                animalArray.add(animalTypes.get(currentAnimal.getAnimalTypeID()));
+            }
+            animalsMap.put(animalArray.get(i), currentAnimal.getIdAnimal());
+
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_spinner_item, animalArray);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        return view;
     }
 }
